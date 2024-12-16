028da480f241c085bd2fc4ddb56fde9aa669a1a1
edwardalx <eobkwaku@gmail.com>
2024-12-16 12:42:07 +0000

    beforeLoops

--- AnglesApp.py
+++ AnglesApp.py
@@ -1,14 +1,14 @@
-Angle = float(input("What type of Angle is ... degrees?"))
+Angle = float(input("What type of Angle is ... degrees?" ))
 
-if Angle < 90:
+if  Angle < 90:
     print("Acute Angle")
-if Angle == 90:
+elif Angle == 90:
     print("Right-Angle")
-if Angle < 180 and Angle > 90:
+elif Angle < 180 :
     print("Obtuse-Angle")
-if Angle == 180:
+elif Angle == 180:
     print("Straight Line")
-if Angle < 360 and Angle > 180:
+elif Angle < 360 :
     print("Reflex-Angle")
-if Angle == 360:
+elif Angle == 360:
     print("Circle")
