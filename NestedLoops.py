1c44520016c7b7ac37ca15213462c2b71c15b4b0
edwardalx <eobkwaku@gmail.com>
2024-12-17 20:06:40 +0000

    NestedLoop& C:/Users/obedd/AppData/Local/Programs/Python/Python313/python.exe c:/Users/obedd/AlxBackEndDev/MyPracticeWork/PythonPractice/NestedLoops.py

---
+++ NestedLoops.py
@@ -0,0 +1,16 @@
+# for i in range(1,6):
+#     for j in range(1,6):
+#         #print("i=", i, "j=", j)
+#         print(f"{i*j:4}", end = "*")
+#     print()
+
+for i  in range(1,3):
+    for j in range(i) :
+        print( i,j ,  end="")
+    print()
+
+# for i  in range(1,3):
+#     for j in range(i) :
+#         print(f"{i:4} {j}",  end="")
+#     print()
+        
