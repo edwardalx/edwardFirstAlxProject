f058d9501521073561af408224c1b8fa9b31de06
edwardalx <eobkwaku@gmail.com>
2025-02-10 23:35:15 +0000

    beforeDjango

--- DataStructure/inbuiltFunction.py
+++
@@ -1,8 +0,0 @@
-setA = {15, 17,10,18,23,}
-setA.update({2,3,10,17})
-print(f"Testing update {setA}")
-
-listA = list(setA)
-print(listA)
-listA.insert(7,22222)
-print((listA))
--- IfandMatchCases/BooksProject.py
+++
@@ -1,7 +0,0 @@
-book_stack = ["Book A", "Book B", "Book C", "Book D"]
-desired_book = input("Enter book title: ")
-#desired_book in book_stack
-if desired_book in book_stack:
-    print("book found")
-else: 
-    print("book not found")
